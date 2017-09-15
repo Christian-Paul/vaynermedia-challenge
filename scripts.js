086f730e15232437137deca07054463b8681b390
Christian <christiansamuelpaul@gmail.com>
2017-09-15 17:57:50 -0400

    fill table with album data from api

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ $(function() {
 
 	const apiAddr = 'https://jsonplaceholder.typicode.com';
 
+	
 	// get user and album data concurrently 
 	$.when(
 		$.get(apiAddr + '/users/1'),
@@ -15,6 +16,25 @@ $(function() {
 		// until all data is received, preventing an inaccurate UI
 
 		console.log('all data: ', user1, user2, album1, album2);
-	})
 
+		for(let i = 0; i < album1[0].length; i++) {
+			$('main > div.table:first-child')
+				.append(
+					`<div class='table__row'>
+						<div class='table__cell table__cell--short'>${album1[0][i]['id']}</div>
+						<div class='table__cell'>${album1[0][i]['title']}</div>
+					</div>`
+				);
+		}
+
+		for(let i = 0; i < album2[0].length; i++) {
+			$('main > div.table:last-child')
+				.append(
+					`<div class='table__row'>
+						<div class='table__cell table__cell--short'>${album2[0][i]['id']}</div>
+						<div class='table__cell'>${album2[0][i]['title']}</div>
+					</div>`
+				);
+		}
+	})
 });
