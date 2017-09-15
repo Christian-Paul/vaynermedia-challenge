d717814320f8f2e20510e36badc26a26be291153
Christian <christiansamuelpaul@gmail.com>
2017-09-15 17:27:38 -0400

    add calls to obtain data concurrently

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 $(function() {
-    console.log('ready!');
+
+	const apiAddr = 'https://jsonplaceholder.typicode.com';
+
+	// get user and album data concurrently 
+	$.when(
+		$.get(apiAddr + '/users/1'),
+		$.get(apiAddr + '/users/2'),
+		$.get(apiAddr + '/albums?userId=1'),
+		$.get(apiAddr + '/albums?userId=2')
+	).done((user1, user2, album1, album2) => {
+
+		// when all data is obtained, then update UI
+		// this is so the UI doesn't start displaying data
+		// until all data is received, preventing an inaccurate UI
+
+		console.log('all data: ', user1, user2, album1, album2);
+	})
+
 });
